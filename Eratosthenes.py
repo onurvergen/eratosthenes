771696eaf9c2d672d4ee9124962edb269fce7527
ONUR <onurergen5@outlook.com>
2018-01-14 17:26:29 +0300

    ilk commit

---
+++ Eratosthenes.py
@@ -0,0 +1,32 @@
+asalsayac = 2
+asaldizi = []
+elenecekdizi = []
+sayi = input("Sayi girin:")
+sayi = int(sayi)
+
+while(asalsayac <= sayi):
+    if(not elenecekdizi):
+        asaldizi.append(asalsayac)
+        asaltut = asalsayac
+        asaltut += asalsayac
+        while (asaltut <= sayi):
+            elenecekdizi.append(asaltut)
+            asaltut += asalsayac
+        asalsayac +=1
+    else:
+        for kontrol in elenecekdizi:
+            if(kontrol == asalsayac):
+                if(asalsayac <= sayi):
+                    asalsayac +=1
+                    break
+        if(asalsayac not in elenecekdizi and asalsayac < sayi):
+            asaldizi.append(asalsayac)
+            asaltut = asalsayac
+            asaltut += asalsayac
+            while (asaltut <= sayi):
+                elenecekdizi.append(asaltut)
+                asaltut += asalsayac
+            asalsayac +=1
+
+for yaz in asaldizi:
+    print(yaz)
